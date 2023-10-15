2fdd49d6834cf291dc848258a60033a6423e4120
Junaid <junaidhayat33303@gmail.com>
2023-10-15 22:23:00 +0500

    nav and header section  done

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,12 @@
-import "../styles/globals.css"
+import "@styles/globals.css"
+import Nav from "@components/Nav"
+import Provider from "@components/Provider"
 
 export const metadata = {
     title: "Promptopia",
     description: "Discover & Sahre AI Prompts"
 }
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
     return (
         <html lang='en'>
             <body>
@@ -12,6 +14,7 @@ const RootLayout = ({children}) => {
                     <div className='gradient' />
                 </div>
                 <main className='app'>
+                    <Nav />
                     {children}
                 </main>
             </body>
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Feed from "@components/Feed"
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='w-full flex-center flex-col'>
+            <h1 className='head_text text-center'>
+                Discover & Share
+                <br className='max-md:hidden' />
+                <span className='orange_gradient text-center'>
+                    AI-Powered Prompts
+                </span>
+            </h1>
+            <p className='desc text-center'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint blanditiis at molestiae culpa cumque magni quisquam dolorum debitis quos ut, vel maiores temporibus nihil et?
+            </p>
+
+            {/* Feed */}
+            <Feed />
+        </section>
     )
 }
 
